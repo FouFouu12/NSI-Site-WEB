3655948b7c9ab1acd0a570e6dc3bf11ab2576547
FouFouu12 <abifadelrani@gmail.com>
2023-12-28 22:35:41 +0200

    updated index.js
    made the popup appear once per session

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,9 @@
-let pop_up = localStorage.getItem('pop-up');
+let pop_up = sessionStorage.getItem('pop-up');
 
 if (!pop_up) {
     window.onload = function () {
         alert("Plus tard, cherchez Martin le Lutin, il pourrait vous aider à retourner à la page précédente!");
     }
 
-    localStorage.setItem('pop-up','True')
+    sessionStorage.setItem('pop-up','True')
 }
