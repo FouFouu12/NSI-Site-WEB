420148210af54b96323415a3de9a58f7cba7bbfc
Han Tousseul <marc.abouchar@gmail.com>
2023-12-28 21:42:04 +0200

    fixed an issue with the initial pop-up

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,9 @@
-window.onload = function() {
-    alert("Plus tard, cherchez Martin le Lutin, il pourrait vous aider à retourner à la page précédente!");
+let pop_up = localStorage.getItem('pop-up');
+
+if (!pop_up) {
+    window.onload = function () {
+        alert("Plus tard, cherchez Martin le Lutin, il pourrait vous aider à retourner à la page précédente!");
+    }
+
+    localStorage.setItem('pop-up','True')
 }
